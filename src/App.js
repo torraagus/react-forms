07d0a96fb5499d7c2195f8ce1fe825a5ecf6b43b
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-05 12:56:55 -0300

    change tag h2 content from 'Simple Form' to 'Login'

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import HookForm from "./components/hookForm/hookForm.component";
 import "./App.scss";
 
-export default class App extends Component {
-
+export default class App extends Component {  
+  
   render() {
     return (
       <div>
-        <h2>Simple Form</h2>
+        <h2>Login</h2>
         <HookForm />
       </div>
     );
