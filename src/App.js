3f28b864ad3a4ad0e3c4c3c22594680e81d7a4ce
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-06 10:19:34 -0300

    import navbar component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React, { Component } from "react";
-import LoginForm from "./components/loginForm/loginForm.component";
 import { Grid, Row, Col } from "react-flexbox-grid";
 
 import "./App.scss";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import LoginForm from "./components/loginForm/loginForm.component";
+import NavBar from "./components/navBar/navbar.component";
 
 export default class App extends Component {
   render() {
     return (
-      <Grid fluid className="mt-5">
-        <Row center="xs">
-          <Col>
-            <h2>Login</h2>
-          </Col>
-        </Row>
-        <Row center="xs">
-          <Col>
-            <p>Complete the form below please</p>
-          </Col>
-        </Row>
-        <Row center="xs" className="mt-3">
-          <Col>
-            <LoginForm />
-          </Col>
-        </Row>
-      </Grid>
+      <>
+        <NavBar />
+        <Grid fluid className="mt-5">
+          <Row center="xs">
+            <Col>
+              <h2>Login</h2>
+            </Col>
+          </Row>
+          <Row center="xs">
+            <Col>
+              <p>Complete the form below please</p>
+            </Col>
+          </Row>
+          <Row center="xs" className="mt-3">
+            <Col>
+              <LoginForm />
+            </Col>
+          </Row>
+        </Grid>
+      </>
     );
   }
 }
