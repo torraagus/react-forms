e796f6897c2ff15ba4dbdd3b5d350bfb1617bbac
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-07 17:13:20 -0300

    create component

---
+++ src/components/registerForm/registerForm.component.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Grid, Row, Col } from "react-flexbox-grid";
+
+export default function RegisterForm() {
+  return (
+    <Grid fluid className="mt-5">
+      <Row center="xs">
+        <Col>
+          <h2>Register</h2>
+        </Col>
+      </Row>
+      <Row center="xs">
+        <Col>
+          <p>Complete the form below please</p>
+        </Col>
+      </Row>
+    </Grid>
+  );
+}
