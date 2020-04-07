bf93ca2582872056c52d1d64647f672e952669ff
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-07 17:07:37 -0300

    import react-router-dom and react-router-bootstrap for navigation

--- src/components/navBar/navbar.component.js
+++ src/components/navBar/navbar.component.js
@@ -2,29 +2,49 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Form from "react-bootstrap/Form";
+import { Switch, Route, Link } from "react-router-dom";
+import { LinkContainer, IndexLinkContainer } from "react-router-bootstrap";
+
+import RegisterForm from "../registerForm/registerForm.component";
+import LoginForm from "../loginForm/loginForm.component";
+import Home from "../home/home.component";
 
 export default function NavBar() {
   return (
-    <Navbar bg="primary" variant="dark" expand="lg">
-      <Navbar.Brand href="#home">React-Forms</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <NavDropdown title="Pick one" id="basic-nav-dropdown">
-            <small className="ml-3"><i>General</i></small>
-            <NavDropdown.Item href="#action/3.1">Login form</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Register form
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <small className="ml-3"><i>Others</i></small>
-            <NavDropdown.Item href="#action/3.3">New seller</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.4">New delivery</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Navbar expand="lg">
+        <Navbar.Brand as={Link} to="/">
+          React-Forms
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <IndexLinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </IndexLinkContainer>
+            <NavDropdown title="Pick a form" id="basic-nav-dropdown">
+              <NavDropdown.Header>General</NavDropdown.Header>
+              <LinkContainer to="/login">
+                <NavDropdown.Item>Login form</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/register">
+                <NavDropdown.Item>Register form</NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Divider />
+              <NavDropdown.Header>Others</NavDropdown.Header>
+              <NavDropdown.Item href="#action/3.3">New seller</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                New delivery
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Switch>
+        <Route exact path="/register" component={RegisterForm} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </>
   );
 }
