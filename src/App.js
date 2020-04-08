398b0f4cf9c49bd0d6ddcd725016a5c8405baf10
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-08 17:11:01 -0300

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import { Grid, Row, Col } from "react-flexbox-grid";
 import { BrowserRouter } from "react-router-dom";
 
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import LoginForm from "./components/loginForm/loginForm.component";
 import NavBar from "./components/navBar/navbar.component";
 
 export default class App extends Component {
