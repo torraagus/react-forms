1feb146da292c53e7b21a90e0afecc182155a7eb
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-08 20:20:00 -0300

    fix tests

--- src/components/loginForm/__tests__/loginForm.test.js
+++ src/components/loginForm/__tests__/loginForm.test.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { render, cleanup, fireEvent, act } from "@testing-library/react";
 import renderer from "react-test-renderer";
-import { act } from "react-dom/test-utils";
 import "@testing-library/jest-dom/extend-expect";
 
 import LoginForm from "../loginForm.component";
@@ -20,49 +19,73 @@ it("matches snapshot", () => {
   expect(tree).toMatchSnapshot();
 });
 
-test("calls onSubmit with the username and password when submit is clicked", async () => {
+test("calls onSubmit with the email and password when submit is clicked", async () => {
   const { getByTestId } = render(<LoginForm />);
-  const user = {username: 'test@gmail.com', password: 'passtest'}
+  const user = {email: 'test@gmail.com', password: 'passtest'}
   
-  await fireEvent.change(getByTestId("username"), { target: { value: user.username } });
-  expect(getByTestId("username").value).toBe(user.username);
-  
-  await fireEvent.change(getByTestId("password"), {
-    target: { value: user.password },
+  await act( async () => {
+    fireEvent.change(getByTestId("email"), { target: { value: user.email } });
+  });
+  expect(getByTestId("email").value).toBe(user.email);
+
+  await act( async () => {
+    fireEvent.change(getByTestId("password"), {
+      target: { value: user.password },
+    });
   });
   expect(getByTestId("password").value).toBe(user.password);
   
-  await fireEvent.submit(getByTestId("form"));
+  await act( async () => {
+    fireEvent.submit(getByTestId("form"));
+  });
+  expect(getByTestId("emailErrors").textContent).toBe("");
+  expect(getByTestId("passwordErrors").textContent).toBe("");
 });
 
-test("shows an error message when submit is clicked and no username is provided", async () => {
+test("shows an error message when submit is clicked and no email is provided", async () => {
   const { getByTestId } = render(<LoginForm />);
 
-  await fireEvent.submit(getByTestId("form"));
-  expect(getByTestId("username").value).toBe("");
+  await act( async () => {
+    fireEvent.submit(getByTestId("form"));
+  });
+  expect(getByTestId("email").value).toBe("");
+  expect(getByTestId("emailErrors").textContent).toBe("The email field is required");
 });
 
-test("shows an error message when submit is clicked and username format is wrong", async () => {
+test("shows an error message when submit is clicked and email format is wrong", async () => {
   const { getByTestId } = render(<LoginForm />);
 
-  await fireEvent.change(getByTestId("username"), { target: { value: "test" } });
-  expect(getByTestId("username").value).toBe("test");
+  await act( async () => {
+    fireEvent.change(getByTestId("email"), { target: { value: "test" } });
+  });
+  expect(getByTestId("email").value).toBe("test");
   
-  await fireEvent.submit(getByTestId("form"));
+  await act( async () => {
+    fireEvent.submit(getByTestId("form"));
+  });
+  expect(getByTestId("emailErrors").textContent).toBe("The email format is wrong");
 });
 
 test("shows an error message when submit is clicked and no password is provided", async () => {
   const { getByTestId } = render(<LoginForm />);
 
-  await fireEvent.submit(getByTestId("form"));
+  await act( async () => {
+    fireEvent.submit(getByTestId("form"));
+  });
   expect(getByTestId("password").value).toBe("");
+  expect(getByTestId("passwordErrors").textContent).toBe("The password field is required");
 });
 
 test("shows an error message when submit is clicked and the password provided have less than 8 characters", async () => {
   const { getByTestId } = render(<LoginForm />);
   
-  await fireEvent.change(getByTestId("password"), { target: { value: "pass" } });
+  await act( async () => {
+    fireEvent.change(getByTestId("password"), { target: { value: "pass" } });
+  });
   expect(getByTestId("password").value).toBe("pass");
   
-  await fireEvent.submit(getByTestId("form"));
+  await act( async () => {
+    fireEvent.submit(getByTestId("form"));
+  });
+  expect(getByTestId("passwordErrors").textContent).toBe("The password must have at least 8 characters");
 });
